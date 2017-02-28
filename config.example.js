2c1ccd32f75b66865bd231946d40d196ef437f49
GenUltra758 <gen.ultra758@gmail.com>
2017-02-28 11:21:36 +0100

    added a example config

---
+++ config.example.js
@@ -0,0 +1,7 @@
+//rename this file to config.js and edit the example values below
+module.exports = {
+    ownerID :  '123456789123456789',//add your owner id here
+    botToken : 'bot token here',//add your application token here
+    botID :    '123456789123456789',//add the id of your bot here
+    adminID :  '123456789123456789' //add the admin group id here
+}
