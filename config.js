35d62a9bcca12f54ec9aa55f011ccdd54fcd6574
GeneralUltra758 <gen.ultra758@gmail.com>
2017-02-28 11:05:46 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ownerID : '104674953382612992',
-    botToken : 'MjgxNTg5MDMwNTQwMjc5ODA4.C4aJ1g.zfftCpKUEJfmkcT7lxETbQC-rP4',
-    botID : '281589030540279808',
-    adminID : "249405763049619456"
-}
